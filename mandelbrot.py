47a23009f49cae4c6e33608c58afa4b4fc10ce08
Martin Rother <22077819+Martin-Rother@users.noreply.github.com>
2023-10-20 23:52:06 +0200

    refactoring

--- mandelbrot.py
+++ mandelbrot.py
@@ -10,23 +10,29 @@ ratio_y = 9
 ratio = ratio_x / ratio_y
 rows = config.frame_size[1]
 cols = ceil(rows * ratio)
-height = config.frame_height
-width = height * ratio
+
+""" zoom = 10000000
+center_x = -1.308109
+center_y = -0.062998 """
+""" zoom = 300000
+center_x = -0.51012
+center_y = -0.522997 """
+zoom = 10000
+center_x = -0.34853774148008254
+center_y = -0.6065922085831237
+max_iter = 20000
 
 #@jitclass
 class Mandelbrot(Scene):
 
     def construct(self):
-        global rows
-        global height
-        global width
         global ratio
+        global zoom
+        global center_x
+        global center_y
 
         start_time = time.time()
-            
-        zoom = 10000000
-        center_x = -1.308109
-        center_y = -0.062998
+        
         inv_zoom = 1 / zoom
         inv_zoom_ratio = inv_zoom * ratio
         re_min = center_x - inv_zoom_ratio
@@ -34,12 +40,7 @@ class Mandelbrot(Scene):
         im_min = center_y - inv_zoom
         im_max = center_y + inv_zoom
 
-        print("re_min: ", re_min)
-        print("re_max: ", re_max)
-        print("im_min: ", im_min)
-        print("im_max: ", im_max)
-
-        ax = Axes(
+        """ ax = Axes(
             x_range=[re_min, re_max, inv_zoom],
             y_range=[im_min, im_max, inv_zoom],
             x_length=8 * ratio,
@@ -51,26 +52,29 @@ class Mandelbrot(Scene):
         )
         dot = Dot(ax.coords_to_point(center_x, center_y), color=WHITE)
         label = Text('(' + str(center_x) + ',' + str(center_y) + ')').scale(.5).next_to(dot)
-
+        self.add(ax, dot, label)
+        """
         title = Title(
             "Mandelbrot Set",
             include_underline=False,
             font_size=40,
         ).move_to([-5,3,0])
 
-        pixels_to_render = np.uint8(iterate_rows(re_min, re_max, im_min, im_max, rows))
+        pixels_to_render = np.uint8(iterate_rows(re_min, re_max, im_min, im_max))
         
         image = ImageMobject(pixels_to_render)
-        image.height = height
-        image.width  = width
-        self.add(image, title, ax, dot, label)
+        image.height = config.frame_height
+        image.width  = config.frame_width
+        self.add(image, title)
 
         end_time = time.time()
         elapsed_time = end_time - start_time
         print('Execution time:', elapsed_time, 'seconds')
 
 @njit #(parallel=True)
-def iterate_rows(re_min, re_max, im_min, im_max, rows):
+def iterate_rows(re_min, re_max, im_min, im_max):
+    global rows
+
     pixels = List()
     for y in range(rows):
         pixels.append(iterate_cols(re_min, re_max, im_min, im_max, y))
@@ -83,17 +87,18 @@ def iterate_cols(re_min, re_max, im_min, im_max, y):
     global cols
     
     max_betrag_2 = 4
-    max_iter = 2000
     c_im = im_min + (im_max-im_min)*y/rows
     row = []
     for x in range(cols):
         c_re = re_min + (re_max-re_min)*x/cols
-        iter = julia(c_re, c_im, c_re, c_im, max_betrag_2, max_iter)
+        iter = julia(c_re, c_im, c_re, c_im, max_betrag_2)
         row.append(color_picker(iter, max_iter))
     return row
 
 @njit #(parallel=True)
-def julia(x, y, xadd, yadd, max_betrag_2, max_iter):
+def julia(x, y, xadd, yadd, max_betrag_2):
+    global max_iter
+
     remain_iter = max_iter
     xx = x*x
     yy = y*y
@@ -114,6 +119,6 @@ def julia(x, y, xadd, yadd, max_betrag_2, max_iter):
 @njit #(parallel=True)
 def color_picker(iter, max_iter):
     r = round(iter/max_iter * 255) % 255
-    g = round((iter * 2)/max_iter * 255) % 255
-    b = round((iter * 3)/max_iter * 255) % 255
+    g = round((iter * 3)/max_iter * 255) % 255
+    b = round((iter * 6)/max_iter * 255) % 255
     return [r, g, b, 255]

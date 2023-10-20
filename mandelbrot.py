78a2fd4ae84045cff0733f2b3d4b1a3e449573f9
Martin Rother <22077819+Martin-Rother@users.noreply.github.com>
2023-10-20 13:15:26 +0200

    optimize frame limits

--- mandelbrot.py
+++ mandelbrot.py
@@ -1,8 +1,7 @@
 from manim import *
 from math import log, ceil
 import time
-from joblib import Parallel, delayed
-from numba import jit, njit, prange
+from numba import jit, njit
 from numba.experimental import jitclass
 from numba.typed import List
 
@@ -27,9 +26,24 @@ class Mandelbrot(Scene):
             include_underline=False,
             font_size=40,
         ).move_to([-5,3,0])
+            
+        doube_ratio = ratio * 2
+        zoom = 1
+        center_x = 0
+        center_y = 0
+        delta_x = (doube_ratio / 3)
+        delta_y = 1
+        re_min = ( -center_x - delta_x * 2 ) / zoom
+        re_max = ( -center_x + delta_x ) / zoom
+        im_min = (-center_y - delta_y ) / zoom
+        im_max = ( -center_y + delta_y ) / zoom
 
-        #Parallel(n_jobs=4, prefer="threads")(delayed(self.get_pixels)(y) for y in range(self.rows))
-        pixels_to_render = np.uint8(iterate_rows(rows))
+        print("re_min: ", re_min)
+        print("re_max: ", re_max)
+        print("im_min: ", im_min)
+        print("im_max: ", im_max)
+
+        pixels_to_render = np.uint8(iterate_rows(re_min, re_max, im_min, im_max, rows))
         
         image = ImageMobject(pixels_to_render)
         image.height = height
@@ -41,36 +55,26 @@ class Mandelbrot(Scene):
         print('Execution time:', elapsed_time, 'seconds')
 
 @njit #(parallel=True)
-def iterate_rows(rows):
+def iterate_rows(re_min, re_max, im_min, im_max, rows):
     pixels = List()
     for y in range(rows):
-        pixels.append(iterate_cols(y))
+        pixels.append(iterate_cols(re_min, re_max, im_min, im_max, y))
     return pixels
 
-@njit(parallel=True)
-def iterate_cols(y):
+@njit #(parallel=True)
+def iterate_cols(re_min, re_max, im_min, im_max, y):
     global ratio
     global rows
     global cols
-
-    doube_ratio = ratio * 2
-    zoom = 1
-    center_x = 0
-    center_y = 0
-    delta_x = (doube_ratio / 3)
-    delta_y = 1
-    re_min = ( -center_x - delta_x * 2 ) / zoom
-    re_max = ( -center_x + delta_x ) / zoom
-    im_min = (-center_y - delta_y ) / zoom
-    im_max = ( -center_y + delta_y ) / zoom
+    
     max_betrag_2 = 4
     max_iter = 1000
     c_im = im_min + (im_max-im_min)*y/rows
     row = []
-    for x in prange(cols):
+    for x in range(cols):
         c_re = re_min + (re_max-re_min)*x/cols
         iter = julia(c_re, c_im, c_re, c_im, max_betrag_2, max_iter)
-        row[x] = color_picker(iter, max_iter)
+        row.append(color_picker(iter, max_iter))
     return row
 
 @njit #(parallel=True)

a10101c1967e273991affa68c298feff63531fd3
Martin Rother <22077819+Martin-Rother@users.noreply.github.com>
2023-10-21 05:47:39 +0200

    update algorithms for video rendering

--- mandelbrot.py
+++ mandelbrot.py
@@ -2,43 +2,63 @@ from manim import *
 from math import log, ceil, sqrt
 import time
 from numba import jit, njit
-from numba.experimental import jitclass
 from numba.typed import List
 
 ratio_x = 16
 ratio_y = 9
 ratio = ratio_x / ratio_y
+cols = config.frame_size[0]
 rows = config.frame_size[1]
-cols = ceil(rows * ratio)
 
-""" zoom = 10000000
+""" z = 10000000
 center_x = -1.308109
-center_y = -0.062998 """
-""" zoom = 300000
+center_y = -0.062998 
+max_iter = 10000 """
+""" z = 300000
 center_x = -0.51012
-center_y = -0.522997 """
-zoom = 10000
+center_y = -0.522997 
+max_iter = 1000 """
+""" z = 10000
 center_x = -0.34853774148008254
 center_y = -0.6065922085831237
-max_iter = 20000
+max_iter = 20000 """
+min_zoom = 1
+max_zoom = 10000
+seconds = 10
+center_x = -0.34853774148008254
+center_y = -0.6065922085831237
 
-#@jitclass
 class Mandelbrot(Scene):
 
     def construct(self):
-        global ratio
-        global zoom
-        global center_x
-        global center_y
-
         start_time = time.time()
         
-        inv_zoom = 1 / zoom
-        inv_zoom_ratio = inv_zoom * ratio
-        re_min = center_x - inv_zoom_ratio
-        re_max = center_x + inv_zoom_ratio
-        im_min = center_y - inv_zoom
-        im_max = center_y + inv_zoom
+        i = 0
+        max_iter = 1000
+        for zoom in np.logspace(start=log(min_zoom),stop=log(max_zoom, 10),num=seconds * config.frame_rate, endpoint = False):
+            max_iter = max_iter * 20 ** ((1/config.frame_rate) / seconds)
+            start_loop_time = time.time() 
+            inv_zoom = 1 / zoom
+            inv_zoom_ratio = inv_zoom * ratio
+            re_min = center_x - inv_zoom_ratio
+            re_max = center_x + inv_zoom_ratio
+            im_min = center_y - inv_zoom
+            im_max = center_y + inv_zoom
+            pixels_to_render = np.uint8(iterate_rows(re_min, re_max, im_min, im_max, max_iter))
+            image = ImageMobject(pixels_to_render)
+            image.height = config.frame_height
+            image.width  = config.frame_width
+            self.add(image)
+            self.wait(1/config.frame_rate)
+
+            zoom_string = ("zoom: " + str(zoom)).ljust(27)
+            now = time.time()
+            loop_string = ('loop time: ' + str(now - start_loop_time)).ljust(31)
+            total_string = ('total time: ' + str(now - start_time)).ljust(31)
+            frame_count = ('frame: ' + str(i)).ljust(11)
+            max_iter_string = ('max_iter: ' + str(max_iter)).ljust(31)
+            print(frame_count + zoom_string + loop_string + total_string + max_iter_string)
+            i += 1
 
         """ ax = Axes(
             x_range=[re_min, re_max, inv_zoom],
@@ -52,53 +72,36 @@ class Mandelbrot(Scene):
         )
         dot = Dot(ax.coords_to_point(center_x, center_y), color=WHITE)
         label = Text('(' + str(center_x) + ',' + str(center_y) + ')').scale(.5).next_to(dot)
-        self.add(ax, dot, label)
-        """
+        self.add(ax, dot, label) """  
+
         title = Title(
             "Mandelbrot Set",
             include_underline=False,
             font_size=40,
         ).move_to([-5,3,0])
-
-        pixels_to_render = np.uint8(iterate_rows(re_min, re_max, im_min, im_max))
         
-        image = ImageMobject(pixels_to_render)
-        image.height = config.frame_height
-        image.width  = config.frame_width
-        self.add(image, title)
-
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        print('Execution time:', elapsed_time, 'seconds')
+        self.add(title)
 
 @njit #(parallel=True)
-def iterate_rows(re_min, re_max, im_min, im_max):
-    global rows
-
-    pixels = List()
+def iterate_rows(re_min, re_max, im_min, im_max, max_iter):
+    pixels = []
     for y in range(rows):
-        pixels.append(iterate_cols(re_min, re_max, im_min, im_max, y))
+        pixels.append(iterate_cols(re_min, re_max, im_min, im_max, y, max_iter))
     return pixels
 
 @njit #(parallel=True)
-def iterate_cols(re_min, re_max, im_min, im_max, y):
-    global ratio
-    global rows
-    global cols
-    
+def iterate_cols(re_min, re_max, im_min, im_max, y, max_iter):
     max_betrag_2 = 4
     c_im = im_min + (im_max-im_min)*y/rows
     row = []
     for x in range(cols):
         c_re = re_min + (re_max-re_min)*x/cols
-        iter = julia(c_re, c_im, c_re, c_im, max_betrag_2)
+        iter = julia(c_re, c_im, c_re, c_im, max_betrag_2, max_iter)
         row.append(color_picker(iter, max_iter))
     return row
 
 @njit #(parallel=True)
-def julia(x, y, xadd, yadd, max_betrag_2):
-    global max_iter
-
+def julia(x, y, xadd, yadd, max_betrag_2, max_iter):
     remain_iter = max_iter
     xx = x*x
     yy = y*y
@@ -117,7 +120,7 @@ def julia(x, y, xadd, yadd, max_betrag_2):
     return max_iter - remain_iter
     
 @njit #(parallel=True)
-def color_picker(iter, max_iter):
+def color_picker(iter, max_iter):  
     r = round(iter/max_iter * 255) % 255
     g = round((iter * 3)/max_iter * 255) % 255
     b = round((iter * 6)/max_iter * 255) % 255

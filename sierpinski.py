b18d161493003e45ee077bb902dad9df8c83e62a
Martin Rother <22077819+Martin-Rother@users.noreply.github.com>
2023-10-27 09:36:53 +0200

    add Text to Sierpinski Triangle

--- sierpinski.py
+++ sierpinski.py
@@ -3,24 +3,40 @@ from manim import *
 class Sierpinski(Scene):
 
     plays = {}
+    centers = {}
+    color = RED_C
+    font_size = 50
+    max_depth = 5
 
     def construct(self):
         text = Text("Sierpinski-Triangle", font_size=32, weight=ULTRALIGHT).move_to([4,3,0])
         self.add(text)
 
-        triangle = Polygon([0, 4 * 3**0.5 - 3.5, 0], [-4, -3.5, 0], [4, -3.5, 0], color=GOLD_E, fill_opacity=1, stroke_width=0.5)
+        p1 = [0, 4 * 3**0.5 - 3.5, 0]
+        p2 = [-4, -3.5, 0]
+        p3 = [4, -3.5, 0]
+        triangle = Polygon(p1, p2, p3, color=self.color, fill_opacity=1, stroke_width=0.5)
         self.play(Create(triangle))
         
         self.recursion(triangle)
-
         for play in self.plays.values():
             if (type(play) is list):
                 self.play(*play)
             else:
                 self.play(play)
+        
+        text = Text("Text", font_size=self.font_size, weight=ULTRALIGHT)
+        i = 1
+        for depth_arrays in self.centers.values():
+            if (type(depth_arrays) is list):
+                for center_array in depth_arrays:
+                    self.add(text.copy().scale(i).move_to(center_array))
+            i = i / 2
+        self.wait()
 
-    def recursion(self, triangle, depth = 0, max_depth = 6):
-        if depth > max_depth:
+
+    def recursion(self, triangle, depth = 0):
+        if depth > self.max_depth:
             return
         p1, p2, p3 = triangle.get_vertices()
 
@@ -35,17 +51,19 @@ class Sierpinski(Scene):
         m2 = [m2x, m2y, 0]
         m3 = [m3x, m3y, 0]
 
-        t1_neu = Polygon(p1, m1, m3, color=GOLD_E, fill_opacity=1, stroke_width=0.5)
-        t2_neu = Polygon(p2, m1, m2, color=GOLD_E, fill_opacity=1, stroke_width=0.5)
-        t3_neu = Polygon(p3, m2, m3, color=GOLD_E, fill_opacity=1, stroke_width=0.5)
-        group = Group(t1_neu, t2_neu, t3_neu)
+        t1_new = Polygon(p1, m1, m3, color=self.color, fill_opacity=1, stroke_width=0.5)
+        t2_new = Polygon(p2, m1, m2, color=self.color, fill_opacity=1, stroke_width=0.5)
+        t3_new = Polygon(p3, m2, m3, color=self.color, fill_opacity=1, stroke_width=0.5)
+        group = Group(t1_new, t2_new, t3_new)
 
-        if (depth in self.plays):
-            self.plays[depth].append(FadeTransform(triangle, group))
-        else:
+        if (depth not in self.plays):
             self.plays[depth] = []
-            self.plays[depth].append(FadeTransform(triangle, group))
+        self.plays[depth].append(FadeTransform(triangle, group))
 
-        self.recursion(t1_neu, depth + 1)
-        self.recursion(t2_neu, depth + 1)
-        self.recursion(t3_neu, depth + 1)
+        if (depth not in self.centers):
+            self.centers[depth] = []
+        self.centers[depth].append(center_of_mass([m1, m2, m3]))
+
+        self.recursion(t1_new, depth + 1)
+        self.recursion(t2_new, depth + 1)
+        self.recursion(t3_new, depth + 1)
